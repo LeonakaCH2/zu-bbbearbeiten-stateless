a2dbc0ae038fae21b4921941b494c316bf6fd007
LeonakaCH2 <l.tiefenthaler.inf22@stud.bbbaden.ch>
2025-05-20 14:59:33 +0200

    added release asset etc.

--- main.py
+++ main.py
@@ -33,3 +33,6 @@ def get_csv():
         mimetype="text/csv",
         headers={"Content-disposition": "attachment; filename=zu-bbbearbeiten.csv"},
     )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
